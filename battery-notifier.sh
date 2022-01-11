e734b6cd06c52ccf7ce33bf4324f2cec472a7d8e
Tushar Asodariya <tusharasodariya1@gmail.com>
2022-01-11 11:33:47 +0530

    optimised to show notification only once

--- battery-notifier.sh
+++ battery-notifier.sh
@@ -8,9 +8,7 @@ while true; do
     if on_ac_power; then                   #check if AC is plugged in
         if [ $battery_level -ge 95 ]; then #check if the battery level is over 90%
             notify-send -u critical "Please, kindly unplug your AC adapter" "Battery level: ${battery_level}%" -i battery-full-charged
-        elif [ $battery_level -le 20 ]; then
-            notify-send -u critical "Thank you!!!" "You did an excellent job, Keep it up." -i battery-caution-charging
-            sleep 900 #sleep as charging upto 20 take 15 min max
+            break
         fi
         #if [ $ac_adapter -eq "off" ]; then
         #    notify-send -u critical "AC Adapter" "The AC Adapter is off."
@@ -21,12 +19,10 @@ while true; do
     if [ "$ac_adapter" == "off" ]; then
         if [ $battery_level -le 20 ]; then #check if the battery level is over 90%
             notify-send -u critical "Please, kindly plug-in your AC adapter" "Battery level: ${battery_level}%" -i battery-caution
-        else
-            notify-send -u critical "Thank you!!!" "You did an excellent job, Keep it up." -i face-angel
-            sleep 7200 # sleep as charging is above 95
+            break
         fi
     fi
-    sleep 5 #wait for 300 seconds before checking again
+    sleep 30 #wait for 30 seconds before checking again
 
 done
 
