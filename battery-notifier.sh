18b4f06eb5c6e79d2e47295d160c4d640d25cc35
Atul Anand Oraon <98654618+OAtulA@users.noreply.github.com>
2022-02-26 20:52:28 +0530

    Improved the indentation.

--- battery-notifier.sh
+++ battery-notifier.sh
@@ -23,28 +23,28 @@ while true; do
     if [ "$ac_adapter" == "off" ]; then
         if [ $battery_level -le 20 ]; then #check if the battery level is over 90%
             notify-send -u critical "Please, kindly plug-in your AC adapter" "Battery level: ${battery_level}%" -i battery-caution
-	        # To get the sound notification
-	        aplay sounds/mixkit-wrong-answer-fail-notification-946.wav
+	    # To get the sound notification
+	    aplay sounds/mixkit-wrong-answer-fail-notification-946.wav
             # break
             # sleep 180
         elif [ $battery_level -le 30 ]; then
             notify-send -u critical "Charge it man its ${battery_level}%" -i battery-caution
 
-	        # To get the sound notification
-	        aplay sounds/mixkit-bell-notification-933.wav
+	    # To get the sound notification
+	    aplay sounds/mixkit-bell-notification-933.wav
             # break
             sleep 200
         elif [ $battery_level == 40 ]; then
             notify-send -u critical "Its ${battery_level}%" -i face-angel
-	        # To get the sound notification
+	    # To get the sound notification
             aplay sounds/mixkit-bell-notification-933.wav
 
             # break
             sleep 180
         elif [ $battery_level -ge 95 ]; then
             notify-send -u critical "Thank you!!!" "You did an excellent job,Now remove the charger." -i face-angel
-	        # To get the sound notification
-	        aplay sounds/mixkit-melodical-flute-music-notification-2310.wav
+	    # To get the sound notification
+	    aplay sounds/mixkit-melodical-flute-music-notification-2310.wav
             sleep 90 # sleep as charging is above 95
         fi
     fi
